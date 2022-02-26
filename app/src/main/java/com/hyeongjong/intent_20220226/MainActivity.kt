f96b9393874f38aae4679f5a0032dc666763eab2
이형종 <hj36879@naver.com>
2022-02-26 10:48:42 +0900

    다른 화면으로 이동 버튼 추가

--- app/src/main/java/com/hyeongjong/intent_20220226/MainActivity.kt
+++ app/src/main/java/com/hyeongjong/intent_20220226/MainActivity.kt
@@ -2,10 +2,15 @@ package com.hyeongjong.intent_20220226
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnMoveToOther.setOnClickListener {
+//            OtherActivity 화면으로 이동
+        }
     }
 }
