48258337e2605981e69cd32cf19d6b57e861ad7a
이형종 <hj36879@naver.com>
2022-02-26 13:44:57 +0900

    SENDTO 기능 예제 - putExtra로 문구 미리 작성 가능

--- app/src/main/java/com/hyeongjong/intent_20220226/MainActivity.kt
+++ app/src/main/java/com/hyeongjong/intent_20220226/MainActivity.kt
@@ -60,6 +60,20 @@ class MainActivity : AppCompatActivity() {
             val myIntent = Intent(Intent.ACTION_CALL, myUri)
             startActivity(myIntent)
         }
+
+        btnCall.setOnClickListener {
+
+//            사용법이 맞게 작성되어도, 권한문제로 앱 강제 종료
+            val inputPhoneNum = edtPhoneNum.text.toString()
+            val myUri = Uri.parse("smsto:${inputPhoneNum}")
+            val myIntent = Intent(Intent.ACTION_SENDTO, myUri)
+
+//            미리 입력할 내용 첨부
+            myIntent.putExtra("sms_body", "이 앱을 공유해주세요!")
+
+            startActivity(myIntent)
+        }
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
